d76024c77efb74e280683898136f5594195a29e9
Marcello Sicbaldi <marcello.sicbaldi2@unibo.it>
2025-02-01 22:50:39 +0100

    Add GGIR call

---
+++ sleep/GGIR_call.R
@@ -0,0 +1,24 @@
+library(GGIR)
+
+data_dir <- "/Users/marcellosicbaldi/PPG_pipelines/GGIR_input/"
+output_dir <- "/Users/marcellosicbaldi/PPG_pipelines/GGIR_output/"
+sleeplog_dir <- "/Users/marcellosicbaldi/PPG_pipelines/GGIR_input/sleeplog/"
+GGIR(
+  datadir = data_dir,
+  outputdir = output_dir,
+  #loglocation = sleeplog_dir,
+  sleepwindowType = "SPT", # since diary reported lights off / lights on
+  #HASPT.algo = "sleep log", # default TIB detection since loglocation is specified
+  HASIB.algo = "vanHees2015", # default SIB detection
+  #=====================
+  # read.myacc.csv arguments
+  #=====================
+  # rmc.firstrow.acc = 2, # first ACC column
+  # rmc.col.time = 1, # time columns
+  # rmc.col.acc = 2:4, # ACC columns
+  # rmc.unit.acc = "mg", # ACC units
+  # rmc.format.time = "%Y-%m-%d %H:%M:%S", # format of Empatica timestamp
+  # rmc.unit.time = "POSIX",
+  # rmc.sf = 64, # Empatica sampling frequency
+  # rmc.origin = "1970-01-01", # origin of Empatica timestamp
+)
