427ed50bacb72968faf2753bcf18d9246ac1e19d
Marcello Sicbaldi <55695116+marcellosicbaldi@users.noreply.github.com>
2025-02-15 12:04:15 +0100

    Update GGIR_call

--- sleep/GGIR_call.R
+++ sleep/GGIR_call.R
@@ -7,18 +7,17 @@ GGIR(
   datadir = data_dir,
   outputdir = output_dir,
   #loglocation = sleeplog_dir,
-  sleepwindowType = "SPT", # since diary reported lights off / lights on
-  #HASPT.algo = "sleep log", # default TIB detection since loglocation is specified
+  sleepwindowType = "SPT", 
   HASIB.algo = "vanHees2015", # default SIB detection
   #=====================
   # read.myacc.csv arguments
   #=====================
-  # rmc.firstrow.acc = 2, # first ACC column
-  # rmc.col.time = 1, # time columns
-  # rmc.col.acc = 2:4, # ACC columns
-  # rmc.unit.acc = "mg", # ACC units
-  # rmc.format.time = "%Y-%m-%d %H:%M:%S", # format of Empatica timestamp
-  # rmc.unit.time = "POSIX",
-  # rmc.sf = 64, # Empatica sampling frequency
+  rmc.firstrow.acc = 2, # first ACC column
+  rmc.col.time = 1, # time columns
+  rmc.col.acc = 2:4, # ACC columns
+  rmc.unit.acc = "mg", # ACC units
+  rmc.format.time = "%Y-%m-%d %H:%M:%S", # format of Empatica timestamp
+  rmc.unit.time = "POSIX",
+  rmc.sf = 64, # Empatica sampling frequency
   # rmc.origin = "1970-01-01", # origin of Empatica timestamp
 )
