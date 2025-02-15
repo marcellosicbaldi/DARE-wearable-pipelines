9b454a3f2eb9ae8950d231b7eb3f96f5025cc0dd
Marcello Sicbaldi <55695116+marcellosicbaldi@users.noreply.github.com>
2025-02-15 23:20:31 +0100

    Added util for ACC SMV

--- utils/compute_acc_SMV.py
+++ utils/compute_acc_SMV.py
@@ -1,4 +1,4 @@
 import numpy as np
 
 def compute_acc_SMV(acc_df):
-    return np.sqrt(acc_df["x"]**2 + acc_df["y"]**2 + acc_df["z"]**2)
+    return np.sqrt(acc_df.iloc[:,0]**2 + acc_df.iloc[:,1]**2 + acc_df.iloc[:,2]**2)
